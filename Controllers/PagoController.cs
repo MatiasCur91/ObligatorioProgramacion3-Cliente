e44e6588c422272618892b45247a84653a765016
avrilsanchez088 <avrilsanchez088@gmail.com>
2025-11-23 19:30:38 -0300

    Informacion Auditoria finalizada

--- Controllers/PagoController.cs
+++ Controllers/PagoController.cs
@@ -11,7 +11,7 @@ namespace ClienteHTTPObligatorio.Controllers
         public PagoController(HttpClient httpClient)
         {
             _httpClient = httpClient;
-            _httpClient.BaseAddress = new Uri("https://localhost:7122/api/");
+            _httpClient.BaseAddress = new Uri("http://localhost:5268/api/");
 
         }
         public IActionResult Index()
